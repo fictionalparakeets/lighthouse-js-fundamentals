04abc898a5d1b6b3df343afde0dde5ea4f32a760
fictionalparakeets <banksmatthew32@gmail.com>
2021-04-28 14:45:48 +0000

    add stretch project rough work

---
+++ js-fundamentals/merge.js
@@ -0,0 +1,11 @@
+function merge(arr1, arr2) {
+  let finalArray = [];
+  arr1.forEach(rating => finalArray.push(rating));
+  arr2.forEach(rating => finalArray.push(rating));
+  return finalArray.sort();
+}
+
+// Tests:
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
