be18cdbdd2977283b9e7cb3485c40f4ec9be29ac
fictionalparakeets <banksmatthew32@gmail.com>
2021-04-28 14:40:44 +0000

    correct error on empty saved file

---
+++ js-fundamentals/concatFunction.js
@@ -0,0 +1,12 @@
+function concat(arr1, arr2) {
+  let finalArray = [];
+  arr1.forEach(rating => finalArray.push(rating));
+  arr2.forEach(rating => finalArray.push(rating));
+  return finalArray
+}
+
+// Tests:
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
