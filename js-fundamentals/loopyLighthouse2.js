eefadd4077518bc1192f2a6f10f16711f9ee91b5
fictionalparakeets <banksmatthew32@gmail.com>
2021-06-21 21:16:07 -0400

    fix minor code error

--- js-fundamentals/loopyLighthouse2.js
+++ js-fundamentals/loopyLighthouse2.js
@@ -1,5 +1,5 @@
 const loopyLighthouse = function(range, multiples, words) {
-  for (let i = range[0]; i < range[1] + 1; i++) {
+  for (let i = range[0]; i <= range[1]; i++) {
     if (i % multiples[0] === 0 && i % multiples[1] === 0) {
       console.log(words[0] + words[1]);
     } else if (i % multiples[1] === 0) {
@@ -15,7 +15,7 @@ const loopyLighthouse = function(range, multiples, words) {
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
 
 /*
-The loopyLighthouse function should log to the console all the numbers from 15 to 90, 
-except replacing multiples of 2 with "Batty", multiples of 5 with "Beacon", 
+The loopyLighthouse function should log to the console all the numbers from 15 to 90,
+except replacing multiples of 2 with "Batty", multiples of 5 with "Beacon",
 and multiples of 2 and 5 with "BattyBeacon"
 */
